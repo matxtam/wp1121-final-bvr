00982bfac836a306520723478118172686920bc5
matxtam <hd.reservoir@gmail.com>
2023-11-28 01:09:32 +0800

    just for test: create players at root page

---
+++ src/app/api/player/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Player } from "@/lib/types/db";
+import { db } from "@/db";
+import { playersTable } from "@/db/schema";
+
+
+export async function POST(req: NextRequest){
+  try{
+    const data = await req.json();
+    //todo: parse the data
+    const { number, name, photo, position, } = data as Player
+    await db
+      .insert(playersTable)
+      .values({
+        number,
+        name,
+        photo,
+        position,
+      })
+      .execute();
+    return NextResponse.json("OK", { status: 200 });
+  } catch (error){
+    console.log(error);
+    return NextResponse.json({ error:"Internal Server Error" }, { status: 400 })
+  }
+}
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -39,7 +39,7 @@ export const playersTable = pgTable(
     photo: varchar("photo", { length: 100 }).notNull(),
     number: varchar("number", { length: 100 }).notNull(),
     position: varchar("position", { length: 100 }).notNull(),
-    useable: boolean("useable").notNull(),
+    useable: boolean("useable").default(true).notNull(),
     personalValue: smallint("personal_value").default(0).notNull(),
     personal2pt: smallint("personal_2pt").default(0).notNull(),
     personalIn2pt: smallint("personal_in2pt").default(0).notNull(),
@@ -163,7 +163,7 @@ export const PlayerRelations = relations(playersTable, ({ many }) => ({
 export const GamePerformanceRelations = relations(
   gamePerformancesTable,
   ({ one }) => ({
-    player: one(playersTable, {
+    players: one(playersTable, {
       fields: [gamePerformancesTable.playerId],
       references: [playersTable.displayId],
     }),
