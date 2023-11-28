19c20376f59085c5005299cd18f3e169eba44e1a
qiannnhui <b09901160@ntu.edu.tw>
2023-11-29 07:14:49 +0800

    ‘signin/out+teamprofile’

--- src/app/homePage/layout.tsx
+++ src/app/homePage/layout.tsx
@@ -1,7 +1,9 @@
+import Navbar from "./_components/Navbar";
 export default function HomePageLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="flex flex-col p-12 gap-12">
-      {children}
+      <Navbar />
+      <div className="grow">{children}</div>
     </main>
   );
 }
