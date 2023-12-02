141701f1f7f01ca3f8bde7f73f1218650a79b594
matxtam <hd.reservoir@gmail.com>
2023-12-02 23:40:47 +0800

    history page: select data function

--- src/app/history/[gameId]/_components/HistoryTable.tsx
+++ src/app/history/[gameId]/_components/HistoryTable.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import { GamePerformance } from "@/lib/types/db";
 
+import Switch from "@/components/ui/switch";
+import { type Dispatch } from "react";
+
 interface performanceWithPlayer extends GamePerformance{
-  players: {name: string; number:string; }
+  player: {name: string; number:string; }
 }
 
 type Props = {
@@ -11,26 +14,61 @@ type Props = {
 }
 
 export default function ({playersOfTheGame}:Props) {
-  const [on, setOn] = useState(false);
-  const [twoPt, setTwoPt] = useState(false);
-  const [threePt, setThreePt] = useState(false);
-  const [ft, setFt] = useState(false);
-  const [foul, setFoul] = useState(false);
-  const [steal, setSteal] = useState(false);
-  const [block, setBlock] = useState(false);
-  const [assist, setAssist] = useState(false);
-  const [defReb, setDefReb] = useState(false);
-  const [offRev, setOffRev] = useState(false);
-  const [turnover, setTurnOver] = useState(false);
-  const [point, setPoint] = useState(false);
+  const titles = ["#", "name", "on time", "2pt", "3pt", "ft", "foul", "steal", "block", "assist", "defReb", "offReb", "turnover", "point"]
+  const contents :{title: string, state: [boolean, Dispatch<SetStateAction<boolean>>|undefined] }[] = [];
+  for(let i in titles){
+    if(i==="0" || i==="1"){
+      contents.push({
+        title: titles[i],
+        state: [true, undefined]
+      })
+    }else{
+      contents.push({
+        title: titles[i],
+        state: useState(false),
+      })
+    }
+  }
 
 
-  return (<div className="grid grid-cols-14">
-  {playersOfTheGame.map((player) => (
-  <>
-  <p>{player.players.number}</p>
-  <p>{player.players.name}</p>
-  </>))}
+  return (<div className="flex flex-row w-full px-12 py-6">
+    <div className="grid grid-cols-14 w-3/4 gap-1">
+
+    {contents.map((content) => (
+      content.state[0] && <p key={content.title}>{content.title}</p>
+    ))}
+
+    {playersOfTheGame.map((player) => (
+    <>
+      <p key={player.player.name}>{player.player.number}</p>
+      <p key={player.player.name}>{player.player.name}  </p>
+      {contents[2] .state[0] && <p key={player.player.name}>{"player on?"}   </p>}
+      {contents[3] .state[0] && <p key={player.player.name}>{player.twoPt}   </p>}
+      {contents[4] .state[0] && <p key={player.player.name}>{player.threePt} </p>}
+      {contents[5] .state[0] && <p key={player.player.name}>{player.ft}      </p>}
+      {contents[6] .state[0] && <p key={player.player.name}>{player.foul}    </p>}
+      {contents[7] .state[0] && <p key={player.player.name}>{player.steal}   </p>}
+      {contents[8] .state[0] && <p key={player.player.name}>{player.block}   </p>}
+      {contents[9] .state[0] && <p key={player.player.name}>{player.assist}  </p>}
+      {contents[10].state[0] && <p key={player.player.name}>{player.defReb}  </p>}
+      {contents[11].state[0] && <p key={player.player.name}>{player.offReb}  </p>}
+      {contents[12].state[0] && <p key={player.player.name}>{player.turnover}</p>}
+      {contents[13].state[0] && <p key={player.player.name}>{player.point}   </p>}
+    </>))}
+    </div>
+    <div className="flex flex-col w-1/4">
+
+      {contents.map((content) => (
+        (content.state[1] !== undefined) &&
+        <div className="flex flex-row items-center p-1 gap-3" key={content.title}>
+          <Switch
+            defaultChecked={content.state[0]}
+            onCheckedChange={() => content.state[1]? content.state[1](!content.state[0]) : {}}
+          />
+          <label>{content.title}</label>
+        </div>
+      ))}
+    </div>
   </div>)
 
 }
--- src/app/history/[gameId]/page.tsx
+++ src/app/history/[gameId]/page.tsx
@@ -1,16 +1,27 @@
 import { db } from "@/db";
 import { eq } from "drizzle-orm";
 import { gamesTable, gamePerformancesTable } from "@/db/schema";
+import HistoryTable from "./_components/HistoryTable";
+import { redirect } from "next/navigation";
+import { publicEnv } from "@/lib/env/public";
+
 
 import Image from "next/image";
 
 export default async function ({ params }:{ params: { gameId:string } }){
   const playersOfTheGame = await db.query.gamePerformancesTable.findMany({
     with: {
-      players: true,
+      player: true,
     },
     where: eq(gamePerformancesTable.gameId, params.gameId),
   })
+  const games = await db.query.gamesTable.findFirst({
+    where: eq(gamesTable.displayId, params.gameId)
+  })
+  if (!games){
+    alert("The game does not exist (but why?)");
+    redirect(`${publicEnv.NEXT_PUBLIC_BASE_URL}/homePage`);
+  }
 
   return (<>
     <aside className="flex flex-col items-center h-screen w-1/5 bg-indigo-100">
@@ -24,15 +35,14 @@ export default async function ({ params }:{ params: { gameId:string } }){
         </Image>
       </figure>
       <div className="flex flex-row">
-        <p>date</p>
-        <span>hashtag</span>
+        <p>{games.date?.toString()}</p>
+        <span>{games.hashtag}</span>
       </div>
-      <a>youtube</a>
+      <a>youtube link</a>
     </aside>
     <section className="flex flex-col items-center w-4/5">
-      <h2>title</h2>
-      <div className="grid">
-      </div>
+      <h2 className="px-12 pt-6">{games.title}</h2>
+      <HistoryTable playersOfTheGame={playersOfTheGame}></HistoryTable>
     </section>
   </>)
 }
