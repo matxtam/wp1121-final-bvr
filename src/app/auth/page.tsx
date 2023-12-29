d90444be61ff6111f7c7602260f9ddc1832ca0fa
amychen <amy@LAPTOP-3B9IIBKC>
2023-12-30 00:53:16 +0800

    debug_type

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,13 +3,26 @@ import { db } from "@/db";
 import { usersTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { publicEnv } from "@/lib/env/public";
-
+import {User} from "@/lib/types/db";
 import AuthForm from "./_components/AuthForm";
 
 export default async function AuthPage() {
   // If signed in, redirect to /
   const session = await auth();
-  const allUsers = await db.select().from(usersTable).execute();
+  const tempAllUsers = await db.select().from(usersTable).execute();
+  const allUsers: User[]  = tempAllUsers.map((user) => ({
+    id: user.id.toString(),
+    name: user.name,
+    displayId: user.displayId,
+    email: user.email,
+    photo: user.photo,
+    fbLink: user.fbLink,
+    igLink: user.igLink,
+    ytLink: user.ytLink,
+    cloudLink: user.cloudLink,
+    provider: user.provider,    
+  }));
+
   if (session?.user?.id) {
     redirect(`${publicEnv.NEXT_PUBLIC_BASE_URL}`);
   }
