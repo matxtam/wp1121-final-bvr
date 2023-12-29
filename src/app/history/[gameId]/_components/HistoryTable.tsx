d25db0efb85882b2c44209b6585bf1659c9970ec
matxtam <hd.reservoir@gmail.com>
2023-12-30 00:35:03 +0800

    排到一半

--- src/app/history/[gameId]/_components/HistoryTable.tsx
+++ src/app/history/[gameId]/_components/HistoryTable.tsx
@@ -18,7 +18,7 @@ type Props = {
 }
 
 export default function History ({playersOfTheGame}:Props) {
-  const titles = ["#", "name", "on time", "2pt", "3pt", "ft", "foul", "steal", "block", "assist", "defReb", "offReb", "turnover", "point"]
+  const titles = ["No.", "name", "on time", "2pt", "3pt", "ft", "foul", "steal", "block", "assist", "defReb", "offReb", "turnover", "point"]
   const contents :{title: string, state: [boolean, Dispatch<SetStateAction<boolean>>|undefined] }[] = [];
   
   titles.forEach((title) => {
@@ -40,47 +40,61 @@ export default function History ({playersOfTheGame}:Props) {
   contents[12].state = useState(false);
   contents[13].state = useState(false);
 
-  const [checkAll, setCheckAll] = useState(false);
-
-  return (<div className="flex flex-row w-full px-12 py-6">
-    <div className="grid grid-cols-14 w-3/4 gap-1">
-
-    {contents.map((content, index) => {
-      if(index === 2 && content.state[0]) return (<div className="flex flex-col w-max" key={content.title+"table"}>
-      <p className="">{content.title}</p>
-      <p className="">P1 P2 P3 P4 OT</p>
-      </div>);
-      else return (
-      content.state[0] ? 
-        <p className="w-max" key={content.title+"table"}>{content.title}</p> 
-      : <div className="w-max" key={content.title+"table"}></div>
-    )})}
-
-    {playersOfTheGame.map((player) => (
-    <React.Fragment key={player.player.name}>
-      <p> {player.player.number}</p>
-      <p> {player.player.name}  </p>
-      {contents[2] .state[0] ? (<div className="flex flex-row w-full">
-        <div className={cn("w-1/4 h-full bg-gray-100", player.onP1 && "bg-batra-200")}></div>
-        <div className={cn("w-1/4 h-full bg-gray-100", player.onP2 && "bg-batra-200")}></div>
-        <div className={cn("w-1/4 h-full bg-gray-100", player.onP3 && "bg-batra-200")}></div>
-        <div className={cn("w-1/4 h-full bg-gray-100", player.onP4 && "bg-batra-200")}></div>
-        <div className={cn("w-1/4 h-full bg-gray-100", player.onOt && "bg-batra-200")}></div>
-      </div>) : <div></div>}
-      {contents[3] .state[0] ? <p> {player.twoPt}   </p> : <div></div>}
-      {contents[4] .state[0] ? <p> {player.threePt} </p> : <div></div>}
-      {contents[5] .state[0] ? <p> {player.ft}      </p> : <div></div>}
-      {contents[6] .state[0] ? <p> {player.foul}    </p> : <div></div>}
-      {contents[7] .state[0] ? <p> {player.steal}   </p> : <div></div>}
-      {contents[8] .state[0] ? <p> {player.block}   </p> : <div></div>}
-      {contents[9] .state[0] ? <p> {player.assist}  </p> : <div></div>}
-      {contents[10].state[0] ? <p> {player.defReb}  </p> : <div></div>}
-      {contents[11].state[0] ? <p> {player.offReb}  </p> : <div></div>}
-      {contents[12].state[0] ? <p> {player.turnover}</p> : <div></div>}
-      {contents[13].state[0] ? <p> {player.point}   </p> : <div></div>}
-    </React.Fragment>))}
-    </div>
+  return (
+  <div className="flex flex-row justify-between w-full px-12 py-6">
+    <table className="w-min gap-1 h-min" >
+      <thead>
+        <tr>
+          {contents.map((content, index) => {
+          // titles
+          if(index === 2 && content.state[0]) return (
+          <th className="flex flex-col w-max" key={content.title+"table"}>
+            <p className="">{content.title}</p>
+            <p className="">P1 P2 P3 P4 OT</p>
+          </th>);
+          else if(index <2) return (<th>
+          <p key={content.title+"table"}>{content.title}</p>
+        </th>)
+          else return (
+          content.state[0] ? 
+            <th className="relative">
+              <div className="flex justify-start items-left absolute bottom-0 left-6">
+                <p className="origin-bottom-left -rotate-45" key={content.title+"table"}>{content.title}</p>
+              </div>
+            </th>
+            : <></>
+          // : <div className="w-max" key={content.title+"table"}></div>
+          )})}
+        </tr>
+      </thead>
+    <tbody>
+      {playersOfTheGame.map((player) => (
+      <tr key={player.player.name} className="odd:bg-muted even:bg-background items-center text-center align-middle">
+        <td><p className="p-2"> {player.player.number}</p></td>
+        <td><p className="p-2"> {player.player.name}  </p></td>
+        {contents[2] .state[0] ? (<td className="flex flex-row items-center justify-center my-2">
+          <div className={cn("w-1/5 h-8 bg-primary", player.onP1 && "bg-muted-foreground")}></div>
+          <div className={cn("w-1/5 h-8 bg-primary", player.onP2 && "bg-muted-foreground")}></div>
+          <div className={cn("w-1/5 h-8 bg-primary", player.onP3 && "bg-muted-foreground")}></div>
+          <div className={cn("w-1/5 h-8 bg-primary", player.onP4 && "bg-muted-foreground")}></div>
+          <div className={cn("w-1/5 h-8 bg-primary", player.onOt && "bg-muted-foreground")}></div>
+        </td>) : <></>}
+        {contents[3] .state[0] ? <td><p className="p-2"> {player.twoPt}   </p></td> : <></>}
+        {contents[4] .state[0] ? <td><p className="p-2"> {player.threePt} </p></td> : <></>}
+        {contents[5] .state[0] ? <td><p className="p-2"> {player.ft}      </p></td> : <></>}
+        {contents[6] .state[0] ? <td><p className="p-2"> {player.foul}    </p></td> : <></>}
+        {contents[7] .state[0] ? <td><p className="p-2"> {player.steal}   </p></td> : <></>}
+        {contents[8] .state[0] ? <td><p className="p-2"> {player.block}   </p></td> : <></>}
+        {contents[9] .state[0] ? <td><p className="p-2"> {player.assist}  </p></td> : <></>}
+        {contents[10].state[0] ? <td><p className="p-2"> {player.defReb}  </p></td> : <></>}
+        {contents[11].state[0] ? <td><p className="p-2"> {player.offReb}  </p></td> : <></>}
+        {contents[12].state[0] ? <td><p className="p-2"> {player.turnover}</p></td> : <></>}
+        {contents[13].state[0] ? <td><p className="p-2"> {player.point}   </p></td> : <></>}
+      </tr>))}
+      </tbody>
+    </table>
     <div className="flex flex-col w-1/4">
+
       <div className="flex flex-row items-center p-1 gap-3">
         <Switch
           defaultChecked={false}
@@ -88,7 +102,7 @@ export default function History ({playersOfTheGame}:Props) {
             contents.forEach((content) => {if(content.state[1])content.state[1](checked);})
           }}
         />
-            <label>View All</label>
+          <label>View All</label>
         </div>
       
       {contents.map((content) => (
