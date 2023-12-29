2ebc04d9e4a375e779d1b2c7dc3e70acc52852a1
qiannnhui <b09901160@ntu.edu.tw>
2023-12-29 11:48:02 +0800

    fix_player_usable_table

--- src/app/settings/players/page.tsx
+++ src/app/settings/players/page.tsx
@@ -3,16 +3,12 @@ import CreatePlayerButton from "./_components/CreatePlayerButton";
 import EditPlayerButton from "./_components/EditPlayerButton";
 import { getPlayers } from "./actions";
 
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import { db } from "@/db";
-import { playersTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 import { publicEnv } from "@/lib/env/public";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import DeletePlayerButton from "./_components/DeletePlayerButton";
 import UsableButton from "./_components/UsableButton";
-import { DataTableDemo } from "./_components/PlayersTable";
 
 export default async function PlayerPage() {
     const session = await auth();
@@ -45,6 +41,34 @@ export default async function PlayerPage() {
             </TableHeader>
             <TableBody>
             {players.map((player) => {
+              if (player.usable)
+                return (
+                  <TableRow key={player.displayId}>
+                    <TableCell align="center">{player.number}</TableCell>
+                    <TableCell align="center">
+                      <Avatar>
+                          <AvatarImage src={player.photo} />
+                          <AvatarFallback className="round" >CN</AvatarFallback>
+                      </Avatar>
+                     </TableCell>
+                    <TableCell align="center">{player.name}</TableCell>
+                    <TableCell align="center">{player.position}</TableCell>
+                    <TableCell align="center">
+                        <EditPlayerButton playerId={player.displayId} name={player.name} photo={player.photo} position={player.position} number={player.number} />
+                    </TableCell>
+                    <TableCell align="center">
+                      <DeletePlayerButton displayId={player.displayId} />
+                    </TableCell>
+                    <TableCell align="center">
+                      <UsableButton displayId={player.displayId} usable={player.usable} />
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+            <TableBody>
+            {players.map((player) => {
+              if (!player.usable)
                 return (
                   <TableRow key={player.displayId}>
                     <TableCell align="center">{player.number}</TableCell>
@@ -70,7 +94,6 @@ export default async function PlayerPage() {
               })}
             </TableBody>
         </Table>
-        {/* <DataTableDemo /> */}
     </div>
   );
 }
